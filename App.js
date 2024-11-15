dc2895b25386b64691c8c7b0a8a3cdf12c16235e
Comfort Ngwenya <>
2024-11-15 10:09:56 +0200

    Calling the recording screen on App

--- App.js
+++ App.js
@@ -4,16 +4,33 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/tabs/HomeScreen';
+import RecordScreen from './src/tabs/RecordScreen';
+import { AudioProvider } from './src/context/AudioContext';
+import { colors } from './src/constants/colors';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Voice Notes' }} />
+    <AudioProvider>
+      <NavigationContainer>
+        <StatusBar style="light" />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: colors.primary,
+            },
+            headerTintColor: colors.background,
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Voice Notes' }} />
+          <Stack.Screen name="Record" component={RecordScreen} options={{ title: 'Record Note' }} />
         </Stack.Navigator>
-        </NavigationContainer>
+      </NavigationContainer>
+      </AudioProvider>
   );
 }
 
